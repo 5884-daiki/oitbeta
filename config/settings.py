f4f1ec4381164f4e7b737524da17bdbf6e6a53e8
daiki <e1x21040@oit.ac.jp>
2023-07-17 20:08:04 +0900

    ディレクトリ構造を変更しました

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/home/daiki/to/oitbeta/snsproject/db.sqlite3',
     }
 }
 #DATABASES = {
