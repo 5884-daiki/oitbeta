ba3bf9a9d8adc5f063eae3598f6ec29b72509fcf
daiki <e1x21040@oit.ac.jp>
2023-07-17 22:56:18 +0900

    ディレクトリ構造を変更しました

--- config/settings.py
+++ config/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path#, os
 import os#要確認
 
 #import environ
-from decouple import config
-from dj_database_url import parse as dburl
+#from decouple import config
+#from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
